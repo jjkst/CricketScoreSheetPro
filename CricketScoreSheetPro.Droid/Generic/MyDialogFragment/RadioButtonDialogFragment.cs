bed21fd5f7e03b1494ce5163491ece1699c22cc3
Karthik Jayaraman <Karthi.Jayaraman@gmail.com>
2018-04-25 17:38:11 -0400

    radio button fragment

--- CricketScoreSheetPro.Droid/Generic/MyDialogFragment/RadioButtonDialogFragment.cs
+++ CricketScoreSheetPro.Droid/Generic/MyDialogFragment/RadioButtonDialogFragment.cs
@@ -5,8 +5,10 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V4.Content;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
@@ -18,10 +20,11 @@ namespace CricketScoreSheetPro.Droid.Generic.MyDialogFragment
         void OnSelectedRadioButton(string title, string inputText);
     }
 
-    public class RadioButtonDialogFragment : DialogFragment
+    public class RadioButtonDialogFragment : DialogFragment 
     {
         private ISelectedRadioButtonListener _callback;
-
+        private RadioButton Individual;
+        private RadioButton Tournament;
         private string _title;
 
         public RadioButtonDialogFragment(ISelectedRadioButtonListener callback, string title)
@@ -36,35 +39,36 @@ namespace CricketScoreSheetPro.Droid.Generic.MyDialogFragment
             LinearLayout.LayoutParams layoutparameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.WrapContent);
             layoutparameters.SetMargins(15, 20, 15, 5);
 
-            RadioButton individual = new RadioButton(this.Activity)
-            {
-                Clickable = true,
-                Checked = true,
-                Text = "Individual Game"
-            };
-            RadioButton tournament = new RadioButton(this.Activity)
-            {
-                Checked = true,
-                Text = "Tournament"
-            };
+            TextView title = new TextView(this.Activity);
+            title.SetText(_title, TextView.BufferType.Normal);
+            title.SetBackgroundColor(new Color(ContextCompat.GetColor(this.Activity, Resource.Color.Title)));
+            title.SetPadding(10, 10, 10, 10);
+            title.Gravity = GravityFlags.CenterHorizontal;
+            title.SetTextColor(Color.Black);
+            title.SetTextSize(ComplexUnitType.Px, 50);
+
+            Individual = new RadioButton(this.Activity) { Text = "Individual Game" };
+            Tournament = new RadioButton(this.Activity) { Text = "Tournament" };
 
             RadioGroup rg = new RadioGroup(this.Activity);
-            rg.AddView(individual);
-            rg.AddView(tournament);
+            rg.AddView(Individual);
+            rg.AddView(Tournament);
+            rg.CheckedChange += Rg_CheckedChange; 
             container.AddView(rg);
 
             AlertDialog.Builder inputDialog = new AlertDialog.Builder(this.Activity);
-            inputDialog.SetTitle(_title);
+            inputDialog.SetCustomTitle(title);
             inputDialog.SetView(container);
-            inputDialog.SetPositiveButton("Save", (senderAlert, args) => {
-                var selected = individual.Id == rg.CheckedRadioButtonId ? "Individual Game" : "Tournament";
-                _callback.OnSelectedRadioButton(_title, selected);
-                Toast.MakeText(this.Activity, "Saved.", ToastLength.Short).Show();
-            });
             inputDialog.SetNegativeButton("Cancel", (senderAlert, args) => {
                 Toast.MakeText(this.Activity, "Canceled.", ToastLength.Short).Show();
             });
             return inputDialog.Show();
         }
+
+        private void Rg_CheckedChange(object sender, RadioGroup.CheckedChangeEventArgs e)
+        {
+            _callback.OnSelectedRadioButton(_title, Individual.Id == e.CheckedId ? "Individual Game" : "Tournament");
+        }
+
     }
 }
--- CricketScoreSheetPro.Droid/Resources/Resource.Designer.cs
+++ CricketScoreSheetPro.Droid/Resources/Resource.Designer.cs
@@ -1231,43 +1231,46 @@ namespace CricketScoreSheetPro.Droid
 			public const int Icon = 2131296333;
 			
 			// aapt resource value: 0x7f09004e
-			public const int abc_background_cache_hint_selector_material_dark = 2131296334;
+			public const int Title = 2131296334;
 			
 			// aapt resource value: 0x7f09004f
-			public const int abc_background_cache_hint_selector_material_light = 2131296335;
+			public const int abc_background_cache_hint_selector_material_dark = 2131296335;
 			
 			// aapt resource value: 0x7f090050
-			public const int abc_btn_colored_borderless_text_material = 2131296336;
+			public const int abc_background_cache_hint_selector_material_light = 2131296336;
 			
 			// aapt resource value: 0x7f090051
-			public const int abc_btn_colored_text_material = 2131296337;
+			public const int abc_btn_colored_borderless_text_material = 2131296337;
 			
 			// aapt resource value: 0x7f090052
-			public const int abc_color_highlight_material = 2131296338;
+			public const int abc_btn_colored_text_material = 2131296338;
 			
 			// aapt resource value: 0x7f090053
-			public const int abc_hint_foreground_material_dark = 2131296339;
+			public const int abc_color_highlight_material = 2131296339;
 			
 			// aapt resource value: 0x7f090054
-			public const int abc_hint_foreground_material_light = 2131296340;
+			public const int abc_hint_foreground_material_dark = 2131296340;
+			
+			// aapt resource value: 0x7f090055
+			public const int abc_hint_foreground_material_light = 2131296341;
 			
 			// aapt resource value: 0x7f090004
 			public const int abc_input_method_navigation_guard = 2131296260;
 			
-			// aapt resource value: 0x7f090055
-			public const int abc_primary_text_disable_only_material_dark = 2131296341;
-			
 			// aapt resource value: 0x7f090056
-			public const int abc_primary_text_disable_only_material_light = 2131296342;
+			public const int abc_primary_text_disable_only_material_dark = 2131296342;
 			
 			// aapt resource value: 0x7f090057
-			public const int abc_primary_text_material_dark = 2131296343;
+			public const int abc_primary_text_disable_only_material_light = 2131296343;
 			
 			// aapt resource value: 0x7f090058
-			public const int abc_primary_text_material_light = 2131296344;
+			public const int abc_primary_text_material_dark = 2131296344;
 			
 			// aapt resource value: 0x7f090059
-			public const int abc_search_url_text = 2131296345;
+			public const int abc_primary_text_material_light = 2131296345;
+			
+			// aapt resource value: 0x7f09005a
+			public const int abc_search_url_text = 2131296346;
 			
 			// aapt resource value: 0x7f090005
 			public const int abc_search_url_text_normal = 2131296261;
@@ -1278,29 +1281,29 @@ namespace CricketScoreSheetPro.Droid
 			// aapt resource value: 0x7f090007
 			public const int abc_search_url_text_selected = 2131296263;
 			
-			// aapt resource value: 0x7f09005a
-			public const int abc_secondary_text_material_dark = 2131296346;
-			
 			// aapt resource value: 0x7f09005b
-			public const int abc_secondary_text_material_light = 2131296347;
+			public const int abc_secondary_text_material_dark = 2131296347;
 			
 			// aapt resource value: 0x7f09005c
-			public const int abc_tint_btn_checkable = 2131296348;
+			public const int abc_secondary_text_material_light = 2131296348;
 			
 			// aapt resource value: 0x7f09005d
-			public const int abc_tint_default = 2131296349;
+			public const int abc_tint_btn_checkable = 2131296349;
 			
 			// aapt resource value: 0x7f09005e
-			public const int abc_tint_edittext = 2131296350;
+			public const int abc_tint_default = 2131296350;
 			
 			// aapt resource value: 0x7f09005f
-			public const int abc_tint_seek_thumb = 2131296351;
+			public const int abc_tint_edittext = 2131296351;
 			
 			// aapt resource value: 0x7f090060
-			public const int abc_tint_spinner = 2131296352;
+			public const int abc_tint_seek_thumb = 2131296352;
 			
 			// aapt resource value: 0x7f090061
-			public const int abc_tint_switch_track = 2131296353;
+			public const int abc_tint_spinner = 2131296353;
+			
+			// aapt resource value: 0x7f090062
+			public const int abc_tint_switch_track = 2131296354;
 			
 			// aapt resource value: 0x7f090008
 			public const int accent_material_dark = 2131296264;
@@ -1359,8 +1362,8 @@ namespace CricketScoreSheetPro.Droid
 			// aapt resource value: 0x7f090040
 			public const int design_bottom_navigation_shadow_color = 2131296320;
 			
-			// aapt resource value: 0x7f090062
-			public const int design_error = 2131296354;
+			// aapt resource value: 0x7f090063
+			public const int design_error = 2131296355;
 			
 			// aapt resource value: 0x7f090041
 			public const int design_fab_shadow_end_color = 2131296321;
@@ -1386,8 +1389,8 @@ namespace CricketScoreSheetPro.Droid
 			// aapt resource value: 0x7f090048
 			public const int design_snackbar_background_color = 2131296328;
 			
-			// aapt resource value: 0x7f090063
-			public const int design_tint_password_toggle = 2131296355;
+			// aapt resource value: 0x7f090064
+			public const int design_tint_password_toggle = 2131296356;
 			
 			// aapt resource value: 0x7f090016
 			public const int dim_foreground_disabled_material_dark = 2131296278;
@@ -1509,11 +1512,11 @@ namespace CricketScoreSheetPro.Droid
 			// aapt resource value: 0x7f09003a
 			public const int switch_thumb_disabled_material_light = 2131296314;
 			
-			// aapt resource value: 0x7f090064
-			public const int switch_thumb_material_dark = 2131296356;
-			
 			// aapt resource value: 0x7f090065
-			public const int switch_thumb_material_light = 2131296357;
+			public const int switch_thumb_material_dark = 2131296357;
+			
+			// aapt resource value: 0x7f090066
+			public const int switch_thumb_material_light = 2131296358;
 			
 			// aapt resource value: 0x7f09003b
 			public const int switch_thumb_normal_material_dark = 2131296315;
